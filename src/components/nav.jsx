c94e764e1824242886d46559f5361878c65e29d1
IsaacToledo123 <223245@ids.upchiapas.edu.mx>
2024-07-30 11:53:38 -0600

    push

--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -25,7 +25,7 @@ const nav = () => {
         </div>
         <ul class="hidden absolute top-1/2 left-1/2 transform -translate-y-1/2 -translate-x-1/2 lg:flex lg:mx-auto lg:flex lg:items-center lg:w-auto lg:space-x-6">
           <li>
-            <a class="text-sm text-gray-400 hover:text-gray-500 hover:text-blue-500 hover:font-semibold" href="/"  >
+            <a class="text-sm text-gray-400 hover:text-gray-500 hover:text-blue-500 hover:font-semibold" href="/User/menu"  >
               Menu
             </a>
           </li>
@@ -114,18 +114,8 @@ const nav = () => {
             </Link>
           </li>
         </ul>
-        <a
-          class="hidden lg:inline-block lg:ml-auto lg:mr-3 py-2 px-6 bg-gray-50 hover:bg-gray-100 text-sm text-gray-900 font-bold  rounded-xl transition duration-200"
-          href="#"
-        >
-          Sign In
-        </a>
-        <a
-          class="hidden lg:inline-block py-2 px-6 bg-blue-500 hover:bg-blue-600 text-sm text-white font-bold rounded-xl transition duration-200"
-          href="#"
-        >
-          Sign up
-        </a>
+    
+   
       </nav>
       <div class="navbar-menu relative z-50 hidden">
         <div class="navbar-backdrop fixed inset-0 bg-gray-800 opacity-25"></div>
